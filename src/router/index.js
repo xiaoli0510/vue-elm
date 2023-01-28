5dafc9c8e64d743ebb38fb4e386d003824a6d35f
xiaoxiao <253451518@qq.com>
2023-01-28 18:51:28 +0800

    商家店铺

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,9 @@ import Forget from '../page/forget/forget.vue'
 import City from '../page/city/city.vue'
 import Msite from '../page/msite/msite.vue'
 import Search from '../page/search/search.vue'
-
-
-
-
+import Shop from '../page/shop/shop.vue'
+import ShopDetail from '../page/shop/shop.vue'
+import FoodDetail from '../page/home/home.vue'
 
 
 const router = createRouter({
@@ -44,6 +43,17 @@ const router = createRouter({
             {
                 path:'',component:Search,alias:['/:geohash','']
             }
+        ]
+    },{
+        path:'/shop',
+        component:Shop,
+        children:[{
+            path:'shopDetail',
+            component:ShopDetail
+        },{
+            path:'foodDetail',
+            component:FoodDetail
+        } 
         ]
     }],
 })
